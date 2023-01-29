aa6d9cbae5283459738f9fadf23ab0fcb898cb7a
Michael Ream <mrmream@gmail.com>
2023-01-29 12:15:39 -0700

    All tests pass now

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+        let repeatedString = "";
+        if (num < 0) {
+            return "ERROR";}
+        for (let i = 0; i < num; i++) {
+          repeatedString += str;
+        }
+        return repeatedString;
+      };
 
-};
+      repeatString("", 10)
 
 // Do not edit below this line
 module.exports = repeatString;
