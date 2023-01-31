b6cb286f0f2cb5277c66f4b167f8e4a0db88c0ff
Michael Ream <mrmream@gmail.com>
2023-01-30 17:09:54 -0700

    All tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    elements.forEach(function(element) {
+      let index = array.indexOf(element);
+      while (index !== -1) {
+        array.splice(index, 1);
+        index = array.indexOf(element);
+      }
+    });
+    return array;
+  };
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
